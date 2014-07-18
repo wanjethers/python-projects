658b14b3ad6c3a8d4360861d636648cb5272da04
wanjethers <wan.jethers@gmail.com>
2014-07-18 18:32:46 -0400

    added blah1 +hello world

---
+++ hello world.py
@@ -0,0 +1,23 @@
+name = input("what is your name?\n> ")
+print("hello " + name)
+answer = input("what is your favorite fruit?\n")
+if answer == "pineapple":
+	print("Thats really cool maybe we should be friends")
+	print("Like really pineapples are awesome")
+	print("did you know that there is an enzyme in pineapples that breaks down meat")
+	print("Its kinda weird but while you eat pineapple, it is slowly breaking down your mouth and eating you")
+	print("Its kinda like revenge")
+else:
+	("well ")
+math = 2 ** 8
+print(math)
+math2 = ((2+9)*3)/5
+print(math2)
+math3 = (1+2+3+4+5+6+7+8+9+10+102949584334285019875093609865)**0
+print(math3)
+math4 = 45-18
+print(math4)
+math5 = (287-23+456+72)/4
+print(math5)
+math6 = 3.14*4.5*4.5
+print(math6)
