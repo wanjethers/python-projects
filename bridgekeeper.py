3dbe21bec14dbb8a51a184670266d44d8a14db50
wanjethers <wan.jethers@gmail.com>
2014-08-08 19:06:34 -0400

    bridgekeeper

---
+++ bridgekeeper.py
@@ -0,0 +1,23 @@
+name = input("What is your name?").lower().strip()
+if name == 'lancelot':
+    color = ask_color
+elif name = 'robin':
+    capital = ask("what is the capital of Assyria?")
+elif name = 'galahad':
+    color = ask_color
+elif name = 'arthor':
+    answer = ask("what is the airspeed velocity of an unladen swallow?")
+        
+
+
+
+def ask(question):
+    answer = input(question)
+    answer =  answer.strip().lower()
+    return answer
+
+def ask_color():
+    return ask("what is your favorite color?")
+
+    
+quest = input("What is your quest?").lower().strip()
