a47db522e1d9254fda43a7333f86bc179d47b7f0
wanjethers <wan.jethers@gmail.com>
2014-08-22 18:36:10 -0400

    Create waffles.py

---
+++ waffles.py
@@ -0,0 +1,11 @@
+import time
+def badgers():
+    badgers = 1
+    while badgers <= 12:
+        print("badgers")
+        badgers += 1
+        time.sleep(0.35)
+def muushroooooooom():
+    for mushroom in range(2):
+        print("mushrooooooomm")
+        time.sleep (0.7)
